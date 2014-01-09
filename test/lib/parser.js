c3fff7cc9354fdb8b78976e83eccda83b6ed73f0
Denis Wolf <dear.mr.dw@gmail.com>
2014-01-09 17:30:33 +0200

    little refactoring

--- test/lib/parser.js
+++ test/lib/parser.js
@@ -6,7 +6,7 @@ describe('Parser', function(){
 		it('should parse line with title and year', function(){
 			var lineWithTitle = "Some Title (1998)\n",
 				parsedJSONObject = [{title:'Some Title', year: 1998}],
-				output = parsers.imdbLikeMovie(lineWithTitle);
+				output = parsers.movies(lineWithTitle);
 
 			assert.deepEqual(output, parsedJSONObject);
 		});
@@ -17,14 +17,14 @@ describe('Parser', function(){
 		it('should extract title and year', function(){
 			var lineWithTitle = "\"Salmon & Pumkin\" (1995) {I Think I Biwanwadwa/Zwazu's Off Dway Off (#3.8)}	1996\n",
 				parsedJSONObject = [{title:'Salmon & Pumkin', year: "1995"}],
-				output = parsers.imdbLikeMovie(lineWithTitle);
+				output = parsers.movies(lineWithTitle);
 			assert.deepEqual(output, parsedJSONObject);
 		});
 
 		it('should parse line for series', function(){
 			var lineWithTitleAndEpisode = "\"Salmon & Pumkin\" (1995) {I Think I do/Or's Off Day Off (#3.8)}	1996\n",
 				parsedJSONObject = [{title:'Salmon & Pumkin', year: "1995"}],
-				output = parsers.imdbLikeMovie(lineWithTitleAndEpisode);
+				output = parsers.movies(lineWithTitleAndEpisode);
 
 			assert.deepEqual(output, parsedJSONObject);
 		});
@@ -33,7 +33,7 @@ describe('Parser', function(){
 			var linesWithTitles = "\"Salmon & Pumkin\" (1995) {I Think I do/Or's Off Day Off (#3.8)}	1996\n" +
 					"Þettwa er ekkert mál (2006)				2006\n",
 				parsedJSONObject = [{"title":"Salmon & Pumkin","year":"1995"},{"title":"Þettwa er ekkert mál","year":"2006"}],
-				output = parsers.imdbLikeMovie(linesWithTitles	);
+				output = parsers.movies(linesWithTitles	);
 
 			assert.deepEqual(output, parsedJSONObject);
 		});

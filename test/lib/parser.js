52d703fadd6124b9ca53a3a675e4e98791d6a2dd
Denis Wolf <dear.mr.dw@gmail.com>
2014-01-10 14:06:29 +0200

    test: better nesting

--- test/lib/parser.js
+++ test/lib/parser.js
@@ -13,29 +13,31 @@ describe('Parser', function(){
 	});
 
 
-	describe('for movies', function(){
-		it('should extract title and year', function(){
-			var lineWithTitle = "\"Salmon & Pumkin\" (1995) {I Think I Biwanwadwa/Zwazu's Off Dway Off (#3.8)}	1996",
-				parsedJSONObject = [{title:'Salmon & Pumkin', year: "1995"}],
-				output = parsers.movies(lineWithTitle);
-			assert.deepEqual(output, parsedJSONObject);
-		});
+	describe('for movie.list', function(){
+		describe('when parse line with movie', function(){
+			it('should extract title and year', function(){
+				var lineWithTitle = "\"Salmon & Pumkin\" (1995) {I Think I Biwanwadwa/Zwazu's Off Dway Off (#3.8)}	1996",
+					parsedJSONObject = [{title:'Salmon & Pumkin', year: "1995"}],
+					output = parsers.movies(lineWithTitle);
+				assert.deepEqual(output, parsedJSONObject);
+			});
 
-		it('should parse line for series', function(){
-			var lineWithTitleAndEpisode = "\"Salmon & Pumkin\" (1995) {I Think I do/Or's Off Day Off (#3.8)}	1996",
-				parsedJSONObject = [{title:'Salmon & Pumkin', year: "1995"}],
-				output = parsers.movies(lineWithTitleAndEpisode);
+			it('should parse line for series', function(){
+				var lineWithTitleAndEpisode = "\"Salmon & Pumkin\" (1995) {I Think I do/Or's Off Day Off (#3.8)}	1996",
+					parsedJSONObject = [{title:'Salmon & Pumkin', year: "1995"}],
+					output = parsers.movies(lineWithTitleAndEpisode);
 
-			assert.deepEqual(output, parsedJSONObject);
-		});
+				assert.deepEqual(output, parsedJSONObject);
+			});
 
-		it('should process multiple lines', function(){
-			var linesWithTitles = "\"Salmon & Pumkin\" (1995) {I Think I do/Or's Off Day Off (#3.8)}	1996\n" +
-					"Þettwa er ekkert mál (2006)				2006\n",
-				parsedJSONObject = [{"title":"Salmon & Pumkin","year":"1995"},{"title":"Þettwa er ekkert mál","year":"2006"}],
-				output = parsers.movies(linesWithTitles	);
+			it('should process multiple lines', function(){
+				var linesWithTitles = "\"Salmon & Pumkin\" (1995) {I Think I do/Or's Off Day Off (#3.8)}	1996\n" +
+						"Þettwa er ekkert mál (2006)				2006\n",
+					parsedJSONObject = [{"title":"Salmon & Pumkin","year":"1995"},{"title":"Þettwa er ekkert mál","year":"2006"}],
+					output = parsers.movies(linesWithTitles	);
 
-			assert.deepEqual(output, parsedJSONObject);
+				assert.deepEqual(output, parsedJSONObject);
+			});
 		});
 	});
 });

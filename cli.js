8c2a77a26cf35a62e90b541b973e7ddee6c26b4e
Denis Wolf <dear.mr.dw@gmail.com>
2014-01-15 10:32:32 +0200

    simplify cli

--- cli.js
+++ cli.js
@@ -4,14 +4,9 @@ var program = require('commander');
 program
 	.version('0.0.1')
 	.usage(': stream | node.js cli.js -t type | stream')
-	.option('-t, --type', 'peg filter to apply')
+	.option('-t, --type [type]', 'peg filter to apply. default: movie', 'movie')
 	.parse(process.argv);
 
 var Import = require('./lib/import');
-
-if(!program.type){
-	console.error('Please, provide type of data to parse. For more info add -h flag');
-	process.exit();
-}
 
 var toggleImport = new Import(process.stdin, program.type, process.stdout);

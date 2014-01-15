93a34b3d26d86d7f0f6836d0a96e9af4e900c2cd
Denis Wolf <dear.mr.dw@gmail.com>
2014-01-15 10:25:52 +0200

    using commander.js wrap cli.js in a fancy way

--- cli.js
+++ cli.js
@@ -1,3 +1,17 @@
+#!/usr/bin/env node
+var program = require('commander');
+
+program
+	.version('0.0.1')
+	.usage(': stream | node.js cli.js -t type | stream')
+	.option('-t, --type', 'peg filter to apply')
+	.parse(process.argv);
+
 var Import = require('./lib/import');
 
-var toggleImport = new Import(process.stdin, 'movie', process.stdout);
+if(!program.type){
+	console.error('Please, provide type of data to parse. For more info add -h flag');
+	process.exit();
+}
+
+var toggleImport = new Import(process.stdin, program.type, process.stdout);
